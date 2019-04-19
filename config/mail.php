6c61099fad6582acf2e845471f4f378031bd2683
Teo Reat <teo.reat@gmail.com>
2019-04-19 23:45:53 +0400

    minor edits 0.01

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'reference@mamatravel.world'),
-        'name' => env('MAIL_FROM_NAME', 'Марьяна Зінченко'),
+        'name' => env('MAIL_FROM_NAME', 'mamatravel.world'),
     ],
 
     /*
